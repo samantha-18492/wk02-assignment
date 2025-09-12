9ab9987777215e3fe8681c6dba8999cea4c7356c
SAMANTHA PARKES <slmillward@outlook.com>
2025-09-12 16:35:54 +0100

    user interaction

---
+++ app.js
@@ -0,0 +1,81 @@
+//images
+
+const thumbnails = document.getElementById("thumbnails");
+const displayContainer = document.getElementById("displayContainer");
+
+const images = [
+  {
+    src: "./assets/clownfish.webp",
+    alt: "A Clown Anemonefish in a Bubble-head Anemone.",
+  },
+  {
+    src: "./assets/coral-reef.webp",
+    alt: "Coral reel life form, reminiscent of mushrooms.",
+  },
+  {
+    src: "./assets/pufferfish.webp",
+    alt: "A Pufferfish resting on the bottom of the sea.",
+  },
+  {
+    src: "./assets/mandarinfish.webp",
+    alt: "A Mandarinfish at the Cairns Aquarium shows of its gorgeous colours and pattern, against a backdrop of white coral.",
+  },
+  {
+    src: "./assets/butterflyfish.webp",
+    alt: "A Cooperband Butterflyfish that lives on coral reefs.",
+  },
+  {
+    src: "./assets/starfish.webp",
+    alt: "A small starfish sitting on a rock.",
+  },
+];
+
+//create thumbnail function
+function createThumbnails() {
+  images.forEach(function (image) {
+    let imageElement = document.createElement("img");
+    imageElement.src = image.src;
+
+    imageElement.addEventListener("click", function () {
+      createBigImage(image);
+    });
+
+    thumbnails.appendChild(imageElement);
+  });
+}
+
+//function invoke
+createThumbnails();
+
+//create larger image function
+function createBigImage(imgDetails) {
+  displayContainer.innerHTML = "";
+  const bigImage = document.createElement("img");
+
+  bigImage.src = imgDetails.src;
+  bigImage.alt = imgDetails.alt;
+
+  displayContainer.appendChild(bigImage);
+}
+
+//buttons
+
+// const leftBtn = document.getElementById("leftBtn");
+// const rightBtn = document.getElementById("rightBtn");
+
+// //create index function
+// function selectNextImage(index) {
+//   currentImageIndex + -index;
+
+//   if (currentImageIndex >= images.length) currentImageIndex = 0;
+//   if (currentImageIndex < 0) currentImageIndex = images.length - 1;
+//   updateDisplayTime(images[currentImageIndex]);
+// }
+
+// leftBtn.addEventListener("click", function () {
+//   selectNextImage(1);
+// });
+
+// rightBtn.addEventListener("click", function () {
+//   selectNextImage(-1);
+// });
